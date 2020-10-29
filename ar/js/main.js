828a87ffac5334aaef2b088d5e8a5f596e691a2c
omarsaleh11 <72376286+omarsaleh11@users.noreply.github.com>
2020-10-29 04:08:09 +0200

    Add files via upload

--- ar/js/main.js
+++ ar/js/main.js
@@ -14,7 +14,7 @@ function closeLanguage() {
 var prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
   var currentScrollPos = window.pageYOffset;
-  if (document.body.scrollTop > 380 || document.documentElement.scrollTop > 200) {
+  if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 100) {
     document.getElementById("footer").style.bottom = "0";
   } else {
     document.getElementById("footer").style.bottom = "-10rem";
@@ -22,3 +22,51 @@ window.onscroll = function() {
   }
   prevScrollpos = currentScrollPos;
 }
+function openCredits() {
+    document.getElementById("img").style.height = "100%";
+}
+function closeCredits() {
+    document.getElementById("img").style.height = "0";
+}
+// slide
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:20,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        300:{
+            items:1.3
+        },
+        320:{
+            items:1.5
+        },
+        500:{
+            items:2
+        },
+        710:{
+            items:2.2
+        },
+        800:{
+            items:2.5
+        },
+        900:{
+            items:3
+        },
+        1050:{
+            items:3.5
+        },
+        1200:{
+            items:4
+        }
+        ,
+        1500:{
+            items:4.5
+        },
+        1700:{
+            items:5
+        }
+    }
+})
