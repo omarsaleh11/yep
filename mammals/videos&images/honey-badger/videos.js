c3e89dd5bd0e2f6f7181a047f698c8f999e764c3
omarsaleh11 <72376286+omarsaleh11@users.noreply.github.com>
2020-11-14 04:31:43 +0200

    Add files via upload

---
+++ mammals/videos&images/honey-badger/videos.js
@@ -0,0 +1,19 @@
+    $('.owl-carousel').owlCarousel({
+        items:1,
+        merge:true,
+        loop:true,
+        margin:10,
+        video:true,
+                URLhashListener:true,
+        autoplayHoverPause:true,
+        startPosition: 'URLHash',
+        center:true,
+        responsive:{
+            480:{
+                items:2
+            },
+            600:{
+                items:4
+            }
+        }
+    })
